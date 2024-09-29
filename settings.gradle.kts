9d8eeab16fda2fbfcb3c045a5cbd1e1ad17b50a9
sryang <sarang628@gmail.com>
2024-09-29 14:05:07 +0800

    update

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://devrepo.kakao.com/nexus/content/groups/public/")
+    }
+}
+
+rootProject.name = "TorangBottomSheet"
+include(":app")
+include(":library")
