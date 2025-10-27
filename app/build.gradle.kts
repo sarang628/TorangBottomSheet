9784af6e843b569ad4f50ddf98885a62357e9245
sryang <sarang628@gmail.com>
2025-10-27 10:18:04 +0900

    update modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.6"
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtension.get()
     }
 
     compileOptions {
